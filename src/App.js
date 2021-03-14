a0cc02895dc3f24bacb2e8d0ab23f94bc23a86a7
woodygaragelab <woodygaragelab@gmail.com>
2021-03-14 12:39:04 +0900

    Item

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listPersons } from './graphql/queries';
-import { createPerson as createPersonMutation, deletePerson as deletePersonMutation } from './graphql/mutations';
+import { listItems } from './graphql/queries';
+import { createItem as createItemMutation, deleteItem as deleteItemMutation } from './graphql/mutations';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -12,37 +12,37 @@ import Card from 'react-bootstrap/Card';
 const initialFormState = { name: '', description: '' }
 
 function App() {
-  const [persons, setPersons] = useState([]);
+  const [items, setItems] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
-  useEffect(() => {  fetchPersons(); }, []);
-  async function fetchPersons() {
-    const apiData = await API.graphql({ query: listPersons });
-    const personsFromAPI = apiData.data.listPersons.persons;
-    await Promise.all(personsFromAPI.map(async person => {
-      if (person.image) {
-        const image = await Storage.get(person.image);
-        person.image = image;
+  useEffect(() => {  fetchItems(); }, []);
+  async function fetchItems() {
+    const apiData = await API.graphql({ query: listItems });
+    const itemsFromAPI = apiData.data.listItems.items;
+    await Promise.all(itemsFromAPI.map(async item => {
+      if (item.image) {
+        const image = await Storage.get(item.image);
+        item.image = image;
       }
-      return person;
+      return item;
     }))
-    setPersons(apiData.data.listPersons.persons);
+    setItems(apiData.data.listItems.items);
   }
-  async function createPerson() {
+  async function createItem() {
     if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createPersonMutation, variables: { input: formData } });
+    await API.graphql({ query: createItemMutation, variables: { input: formData } });
     if (formData.image) {
       const image = await Storage.get(formData.image);
       formData.image = image;
     }
-    setPersons([ ...persons, formData ]);
+    setItems([ ...items, formData ]);
     setFormData(initialFormState);
   }
 
-  async function deletePerson({ id }) {
-    const newPersonsArray = persons.filter(person => person.id !== id);
-    setPersons(newPersonsArray);
-    await API.graphql({ query: deletePersonMutation, variables: { input: { id } }});
+  async function deleteItem({ id }) {
+    const newItemsArray = items.filter(item => item.id !== id);
+    setItems(newItemsArray);
+    await API.graphql({ query: deleteItemMutation, variables: { input: { id } }});
   }
 
   async function onChange(e) {
@@ -50,7 +50,7 @@ function App() {
     const file = e.target.files[0];
     setFormData({ ...formData, image: file.name });
     await Storage.put(file.name, file);
-    fetchPersons();
+    fetchItems();
   }
 
 
@@ -60,21 +60,21 @@ function App() {
       <h1>Tokimeter</h1>
       <div style={{marginBottom: 30}}>
         {
-          persons.map(person => (
+          items.map(item => (
             <Card>
             <Card.Body>
-              {/* <div key={person.id || person.name}> */}
+              {/* <div key={item.id || item.name}> */}
               <div class="container-fluid">
               <div class="row">
                 <div class="col-4">
-                  <img src={person.image} style={{width: 50,height:50}}/>
+                  <img src={item.image} style={{width: 50,height:50}}/>
                 </div>
                 <div class="col-6">
-                  <div>{person.name}</div>
-                  <div>{person.description}</div>
+                  <div>{item.name}</div>
+                  <div>{item.description}</div>
                 </div>
                 <div class="col-2">
-                  <Button onClick={() =>  deletePerson(person)} variant="outline-primary">Delete</Button>
+                  <Button onClick={() =>  deleteItem(item)} variant="outline-primary">Delete</Button>
                 </div>
               </div>              
               </div>              
@@ -87,7 +87,7 @@ function App() {
       <div class="container-fluid">
       <div class="row">
         <div class="col-3">
-          <Button onClick={createPerson} variant="outline-primary">ADD</Button>
+          <Button onClick={createItem} variant="outline-primary">ADD</Button>
         </div>
         <div class="col-3">
           <input
