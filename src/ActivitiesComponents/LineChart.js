061e2e939c4954fda8fd1a7f4274b06b43b1fc86
Bryanwangzb <bryanwzb@163.com>
2021-05-23 17:15:24 +0900

    move raw data process code into function

--- src/ActivitiesComponents/LineChart.js
+++ src/ActivitiesComponents/LineChart.js
@@ -1,48 +1,49 @@
 import React from 'react'
-import {Line} from 'react-chartjs-2'
-import heartRateData from '../BioData/heart_rate_example.json'
+import { Fragment } from 'react'
+import { Line } from 'react-chartjs-2'
 
-const heartRateRawDatas = heartRateData;
 
-// すべての心拍数をbpmListに保存
-const bpmList = heartRateRawDatas.map((s, index) => {
-    return s.value.bpm
+const LineChart = ({heartRateRawData}) => {
 
-})
+    //const heartRateRawDatas = heartRateData;
 
-// Line Chartを描画するための心拍数リストの宣言
-let sampledList = []
-let sampleStep = 60
+    // すべての心拍数をbpmListに保存
+    const bpmList = heartRateRawData.map((s, index) => {
+        return s.value.bpm
 
-// 設定されるステップ（sampleStep）で、サンプルする
-for (let i in bpmList) {
-    if (i % sampleStep === 0) {
-        sampledList.push(bpmList[i])
+    })
+
+    // Line Chartを描画するための心拍数リストの宣言
+    let sampledList = []
+    let sampleStep = 60
+
+    // 設定されるステップ（sampleStep）で、サンプルする
+    for (let i in bpmList) {
+        if (i % sampleStep === 0) {
+            sampledList.push(bpmList[i])
+        }
     }
-}
 
-// debug用
-console.log(sampledList.length)
+    // debug用
+    console.log(sampledList.length)
 
-let labelIndex = 0 
-const labelList = []　// X軸座標格納用リスト
-let timeIndex = 0　// X軸（時間軸座標）初期値は０で、上限24
-while (labelIndex < sampledList.length) {
-    let timeUnit = parseInt(sampledList.length / 24)　// X軸数値（1:00, 2:00 ...）の間隔　
-    console.log(timeUnit)
-    if (labelIndex % timeUnit === 0) {
-        labelList.push(timeIndex + ":00")　// 整除するのみ、X軸上に出力
-        timeIndex++
-        if (timeIndex >= 25) break 
+    let labelIndex = 0
+    const labelList = []　// X軸座標格納用リスト
+    let timeIndex = 0　// X軸（時間軸座標）初期値は０で、上限24
+    while (labelIndex < sampledList.length) {
+        let timeUnit = parseInt(sampledList.length / 24)　// X軸数値（1:00, 2:00 ...）の間隔　
+        console.log(timeUnit)
+        if (labelIndex % timeUnit === 0) {
+            labelList.push(timeIndex + ":00")　// 整除するのみ、X軸上に出力
+            timeIndex++
+            if (timeIndex >= 25) break
+        }
+        else { labelList.push("") }　// X軸数値（1,2 ...）以外の場合、空白を入れる
+        labelIndex++
     }
-    else { labelList.push("") }　// X軸数値（1,2 ...）以外の場合、空白を入れる
-    labelIndex++
-}
-
-const LineChart = () => {
 
 
-    return <div>
+    return (<Fragment>
 
         <Line
             data={{
@@ -63,7 +64,7 @@ const LineChart = () => {
                 maintainAspectRatio: false,
             }}
         />
-    </div>
+    </Fragment>)
 }
 
 
