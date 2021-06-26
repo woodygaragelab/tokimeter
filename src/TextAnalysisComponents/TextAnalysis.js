9e423a547d9ac3b3c3f07986519b955058fa8857
Bryanwangzb <bryanwzb@163.com>
2021-06-26 22:43:00 +0900

    Add text box for member's name

--- src/TextAnalysisComponents/TextAnalysis.js
+++ src/TextAnalysisComponents/TextAnalysis.js
@@ -3,14 +3,17 @@ import React from 'react'
 import { useState } from 'react'
 
 export const TextAnalysis = () => {
-    const url = "https://kojipro.an.r.appspot.com/getscore?text="
+    const baseUrl = "https://kojipro.an.r.appspot.com/getscore?text="
     const [data, setData] = useState('')
     const [result, setResult] = useState('')
+    const [member,setMember] = useState('')
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // デバッグ用
         console.log(data)
+        console.log(member)
 
         if (!data) {
             alert('会話内容を入力ください')
@@ -23,7 +26,7 @@ export const TextAnalysis = () => {
         }
 
         // Kozipro分析結果の取得
-        fetch(url + data)
+        fetch(baseUrl + data)
             .then((response) => {
                 response.json().then(analysisResult => {
                     setResult(analysisResult)
@@ -37,6 +40,8 @@ export const TextAnalysis = () => {
             <div className="container">
                 <form onSubmit={handleSubmit}>
                     <div className='form-group'>
+                        <label>名前 </label>
+                        <input onChange={(e) => setMember(e.target.value)} placeholder='メンバーの名前を入れてね' value={member} id="input-member" className="from-control"/><br></br>
                         <label>分析内容</label>
                         <textarea onChange={(e) => setData(e.target.value)} placeholder='会話内容を入れてね' value={data} id="input-text" className="form-control" />
                     </div>
