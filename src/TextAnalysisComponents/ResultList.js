e100f5deda499b1203cf361faa3a828f79eebd4b
Bryanwangzb <bryanwzb@163.com>
2021-09-02 22:30:53 +0900

    Add comment for future coding.

--- src/TextAnalysisComponents/ResultList.js
+++ src/TextAnalysisComponents/ResultList.js
@@ -3,10 +3,16 @@ import { useHistory, withRouter } from 'react-router-dom';
 
 function ResultList({userName,chatContent,koziproResult}) {
 
-    //alert("分析結果を保存しました")
+    //テスト用　値をパスできるかの確認
     console.log(userName)
     console.log(chatContent)
     console.log(koziproResult)
+
+    //分析結果をサーバーから取得
+
+
+    //新規の分析結果をサーバーに保存
+
 
     return (
         <>
