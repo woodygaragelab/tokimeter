a6013b201459f65f48448df77f1ef169d3c2e22d
Bryanwangzb <bryanwzb@163.com>
2022-01-21 22:42:22 +0900

    change forget password to change password.

--- src/wang_code/components/newPassword.js
+++ src/wang_code/components/newPassword.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom';              // router (画面遷移制御)機能
-import { useHistory } from 'react-router-dom';
-
 import '../../App.css';
 import Header from './headerW'
-
-
-import { Container, Box, TextField, Button } from '@material-ui/core';
+import { Container, Box} from '@material-ui/core';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import pink from '@material-ui/core/colors/pink';
 import { Account } from './Account';
 import ChangePassword from './ChangePassword';
-import SignUp from '../../SignUp';
 
 const theme = createTheme({
     palette: {
@@ -20,15 +15,12 @@ const theme = createTheme({
     },
 });
 
-function forgetPassword() {
+function changePassword() {
 
     return (
         <ThemeProvider theme={theme}>
             <Account>
-
-        
             <Header />
-
             <Container>
                 <Box style={{ display: 'flex', flexDirection: 'row' }}>
                     <Box width={1 / 4} />
@@ -43,17 +35,14 @@ function forgetPassword() {
                             <Account>
                                 <ChangePassword />
                             </Account>
-
                         </form>
                     </Box>
-
                     <Box width={1 / 4} />
                 </Box>
-
             </Container>
             </Account>
         </ThemeProvider>
     );
 }
 
-export default withRouter(forgetPassword) 
+export default withRouter(changePassword) 
