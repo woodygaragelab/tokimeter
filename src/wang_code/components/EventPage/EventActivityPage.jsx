b5db7894886ca5bb75bcff4a4f4db53fed35bc7b
Bryanwangzb <bryanwzb@163.com>
2022-03-08 23:08:20 +0900

    [update] show heart beat graph.

--- src/wang_code/components/EventPage/EventActivityPage.jsx
+++ src/wang_code/components/EventPage/EventActivityPage.jsx
@@ -27,19 +27,26 @@ function EventActivityPage() {
 
     const [bioData, setBioData] = useState();
     const [heartRateData, setHeartrateData] = useState();
-    const [showGraph, setShowGraph] = useState(true);
+    const [showGraph, setShowGraph] = useState(false);
 
     const uploadBioData = async () => {
         // Upload the bio data
         console.log('bioData', bioData)
         // uuid() unique name
         const fileName = `${uuid()}.json`;
-        await Storage.put(fileName, bioData, { contentType: 'application/json' },{ level: 'public' })
+        await Storage.put(fileName, bioData, { contentType: 'application/json' }, { level: 'public' })
         if (bioData) {
             const heartRateDataUrl = await Storage.get(fileName);
-            console.log("Output: ",heartRateDataUrl)
-            setHeartrateData(heartRateData)
-           
+            console.log("Output: ", heartRateDataUrl)
+
+            fetch(heartRateDataUrl)
+                .then(result => result.json())
+                .then((heartRateData) => {
+                    console.log('Output: ', heartRateData);
+                    setHeartrateData(heartRateData)
+                    setShowGraph(true)
+                }).catch(err => console.error(err));
+
         }
 
     }
@@ -53,7 +60,7 @@ function EventActivityPage() {
 
                     <input type="file" accept=".json" onChange={e => setBioData(e.target.files[0])} />
                     <IconButton onClick={uploadBioData}>
-                        Upload Bio Data(Coming soon)
+                        Upload Bio Data
                         <PublishIcon />
                     </IconButton>
 
@@ -62,8 +69,8 @@ function EventActivityPage() {
 
                 <Box sx={{ height: 200 }}>
                     <Box sx={{ background: '#eecccc' }}>
-                       
-                            {/* {showGraph ?(<LineChart heartRateData={heartRateData}/>):"No"} */}
+
+                        {showGraph ?(<LineChart heartRateData={heartRateData}/>):"Upload Your bio Data"}
                     </Box>
 
                 </Box>
