64b6471e23c86d0fe2a1de500f3c25fe3066ec34
Bryanwangzb <bryanwzb@163.com>
2022-04-04 22:07:09 +0900

    Change name textfiled to select.

--- src/wang_code/components/TextPage/TextAnalysisCore.jsx
+++ src/wang_code/components/TextPage/TextAnalysisCore.jsx
@@ -5,8 +5,15 @@ import { Button, TextField, Box, TextareaAutosize, Container } from '@material-u
 import AnalysisResult from '../../../TextAnalysisComponents/AnalysisResult'
 import TextAnalizer from '../../../TextAnalysisComponents/TextAnalizer'
 
+//Select
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+
+
 //sound effects//
-import { useRef } from 'react'; 
+import { useRef } from 'react';
 
 import useSound from 'use-sound';
 import Sound from '../../../sound/shiningsound_1.mp3'
@@ -20,30 +27,26 @@ export const TextAnalysis = () => {
     const [isGetResult, setIsGetResult] = useState(false)
     const [isShowTextAnalizer, setIsShowTextAnalizer] = useState(false)
 
-//sound effects//
+    //sound effects//
     const [score_0, setScore] = useState(50)
     const [play, { stop, pause }] = useSound(Sound);
     const [playLoud] = useSound(Sound, { volume: 2 });
-    
+
     const clickA = () => {
-      setScore(score_0+50);
-      if (audioContext.current.state === "suspended") {
-        audioContext.current.resume();
-      }
-  
-      // context.resume();
-      play();
+        setScore(score_0 + 50);
+        if (audioContext.current.state === "suspended") {
+            audioContext.current.resume();
+        }
+
+        play();
     };
-  
-    // const context = new AudioContext();
-    // useEffect(() => {
-    // }, [])
-    
+
+
     const audioContext = useRef(null);
     useEffect(() => {
-      audioContext.current = new AudioContext();
+        audioContext.current = new AudioContext();
     }, []);
-//sound effects//
+    //sound effects//
 
     // スリープ用
     const sleep = (msec) => {
@@ -85,7 +88,7 @@ export const TextAnalysis = () => {
             return
         }
 
-     
+
         // Kozipro分析結果の取得
         fetchResult();
         setIsShowTextAnalizer(true)
@@ -94,47 +97,63 @@ export const TextAnalysis = () => {
     return (
         <>
             <Container>
-            <Box > 
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <Box>
-                            <TextField
-                                id="input-member"
-                                label="名前"
-                                value={member}
-                                helperText="気になる相手の名前を入力ください"
-                                onChange={(e) => setMember(e.target.value)} />
-                            <Box marginTop={6}>
-                                <TextField
-                                    variant="standard"
-                                    label="会話内容"
-                                    id="input-text"
-                                    multiline
-                                    rows={6}
-                                    onChange={(e) => setData(e.target.value)}
-                                    placeholder='会話内容を入れてね'
-                                    value={data}
-                              
+                <Box >
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            
+                            <Box>
+
+
+                                <Box width="200px" margin="auto">
+                                <InputLabel id="member-simple-select-label">名前</InputLabel>
+                                    <Select
+                                        id="member-simple-select"
+                                        label="名前"
+                                        value={member}
+                                        helperText="気になる相手の名前を選択ください"
+                                        onChange={(e) => setMember(e.target.value)}
+                                        fullWidth
+                                    >
+                                        {/* Names list replaced by server's name list in future. */}
+                                        <MenuItem value="Member1">Member1</MenuItem>
+                                        <MenuItem value="Member2">Member2</MenuItem>
+                                        <MenuItem value="Member3">Member3</MenuItem>
+                                    </Select>
+
+
+                                </Box>
+
+                                <Box marginTop={6}>
+                                    <TextField
+                                        variant="standard"
+                                        label="会話内容"
+                                        id="input-text"
+                                        multiline
+                                        rows={6}
+                                        onChange={(e) => setData(e.target.value)}
+                                        placeholder='会話内容を入れてね'
+                                        value={data}
+
                                     // style={{ width: 600 }} 
                                     />
+                                </Box>
+
                             </Box>
-
-                        </Box>
-                        {!isGetResult && <Box marginTop={3}>
-                            <Button
-                                id="sendText"
-                                variant="contained"
-                                color="secondary"
-                                type="submit" onClick={() => clickA()}>送信</Button>
-                        </Box>}
-                    </div>
-                </form>
+                            {!isGetResult && <Box marginTop={3}>
+                                <Button
+                                    id="sendText"
+                                    variant="contained"
+                                    color="secondary"
+                                    type="submit" onClick={() => clickA()}>送信</Button>
+                            </Box>}
+                        </div>
+                    </form>
                 </Box>
-                </Container>
-                {isShowTextAnalizer && <TextAnalizer></TextAnalizer>}
-                {isGetResult && <AnalysisResult koziproResult={result} objectName={member} objectText={data} />}
+            </Container>
+            {isShowTextAnalizer && <TextAnalizer></TextAnalizer>}
+            {isGetResult && <AnalysisResult koziproResult={result} objectName={member} objectText={data} />}
 
-           
+
 
         </>
     )
