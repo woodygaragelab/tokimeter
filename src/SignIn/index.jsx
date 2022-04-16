5559e33a810600044021ad0e8101ca6509aa5d94
Bryanwangzb <bryanwzb@163.com>
2022-04-16 17:12:54 +0900

    fixed margin.

--- src/SignIn/index.jsx
+++ src/SignIn/index.jsx
@@ -1,6 +1,6 @@
-import React,{useState,useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import { AccountContext } from '../components/Account';
-import { Button,TextField,Box } from '@material-ui/core'
+import { Button, TextField, Box } from '@material-ui/core'
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
@@ -8,55 +8,58 @@ import { Link } from 'react-router-dom';
 
 
 
-function SignIn({onSignIn}) {
+function SignIn({ onSignIn }) {
 
-    const [username,setUsername] = useState(''); // emailが一時的にusernameとして扱う
-    const [password,setPassword] = useState('');
-    const [email,setEmail] = useState('')
-    const history = useHistory()
+  const [username, setUsername] = useState(''); // emailが一時的にusernameとして扱う
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('')
+  const history = useHistory()
 
-    const {authenticate} = useContext(AccountContext)
-
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        authenticate(email, password)
-        .then((data) => {
-          console.log("Logged in!", data);
-        })
-        .catch((err) => {
-          console.error("Failed to sign in", err);
-          alert(err.message || JSON.stringify(err))
-        });
-    };
+  const { authenticate } = useContext(AccountContext)
 
 
-    const selectHome = () => {  history.push({ pathname: '/homepage' });  }
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-        <div>
-            <Box>
-            {/* <TextField label="ID" id="userid" value={username} onChange={e => setUsername(e.target.value)} fullWidth /> */}
-            <TextField label="Email" id="email" value={email} onChange={e => setEmail(e.target.value)} fullWidth />
-            <TextField label="Password" id="password" type='password' value={password} onChange={e => setPassword(e.target.value)} fullWidth />
-            Don't have an account? <Link to='/signuppage'>Sign up</Link>
-            <Box>
-                Forget your password? <Link to='/'>Click here</Link>
-           
-            </Box>
-           
-            </Box>
-            <Box>
-            <Button
-              id='signInButton'   
-              onClick={selectHome}
-              variant="contained" color="secondary" style={{ width: '100%' }}>
-              Login
-            </Button>
-            </Box>
-        </div>
-    )
+    authenticate(email, password)
+      .then((data) => {
+        console.log("Logged in!", data);
+      })
+      .catch((err) => {
+        console.error("Failed to sign in", err);
+        alert(err.message || JSON.stringify(err))
+      });
+  };
+
+
+  const selectHome = () => { history.push({ pathname: '/homepage' }); }
+
+  return (
+    <div>
+      <Box>
+        {/* <TextField label="ID" id="userid" value={username} onChange={e => setUsername(e.target.value)} fullWidth /> */}
+        <TextField margin="normal" label="Email" id="email" value={email} onChange={e => setEmail(e.target.value)} fullWidth autoFocus />
+        <TextField margin="normal" label="Password" id="password" type='password' value={password} onChange={e => setPassword(e.target.value)} fullWidth />
+
+        Don't have an account? <Link to='/signuppage'>Sign up</Link>
+
+
+        <Box>
+          Forget your password? <Link to='/'>Click here</Link>
+
+        </Box>
+
+      </Box>
+      <Box>
+        <Button
+          id='signInButton'
+          onClick={selectHome}
+          variant="contained" color="secondary" style={{ width: '100%' }}>
+          Login
+        </Button>
+      </Box>
+    </div>
+  )
 }
 
 export default SignIn
